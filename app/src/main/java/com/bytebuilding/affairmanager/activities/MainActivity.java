cfedadfbd99e3a9d5c49b390a4f74a8349291228
aLT22 <altsworks@gmail.com>
2016-10-31 11:07:44 +0300

    activity_branch_commit
    added fab button in java-code

--- app/src/main/java/com/bytebuilding/affairmanager/activities/MainActivity.java
+++ app/src/main/java/com/bytebuilding/affairmanager/activities/MainActivity.java
@@ -1,15 +1,30 @@
 package com.bytebuilding.affairmanager.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.bytebuilding.affairmanager.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FloatingActionButton floatingActionButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initComponents();
+    }
+
+    private void initComponents() {
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
