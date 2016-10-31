abd7ea4632d974753538bef3df7812aac37cda1e
aLT22 <altsworks@gmail.com>
2016-10-31 10:50:41 +0300

    activity_branch_commit

--- app/src/main/java/com/bytebuilding/affairmanager/activities/LoginActivity.java
+++ app/src/main/java/com/bytebuilding/affairmanager/activities/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.bytebuilding.affairmanager.activities;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +13,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+
+                LoginActivity.this.startActivity(intent);
+                LoginActivity.this.finish();
+            }
+        }, 2000);
     }
 }
--- app/src/main/java/com/bytebuilding/affairmanager/activities/MainActivity.java
+++ app/src/main/java/com/bytebuilding/affairmanager/activities/MainActivity.java
@@ -5,8 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.bytebuilding.affairmanager.R;
 
-/*something test*/
-//some text
 public class MainActivity extends AppCompatActivity {
 
     @Override
