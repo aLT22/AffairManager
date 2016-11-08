80a0560cab82d04f16b41e315b15aa6b0e6a0218
aLT22 <altsworks@gmail.com>
2016-11-08 12:37:48 +0300

    offline_client_branch commit
    added two fragments which names are CurrentAffairs and DoneAffairs

---
+++ app/src/main/java/com/bytebuilding/affairmanager/fragments/DoneOfflineAffairsFragment.java
@@ -0,0 +1,27 @@
+package com.bytebuilding.affairmanager.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bytebuilding.affairmanager.R;
+
+public class DoneOfflineAffairsFragment extends Fragment {
+
+
+    public DoneOfflineAffairsFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_done_offline_affairs, container, false);
+
+        return rootView;
+    }
+
+}
