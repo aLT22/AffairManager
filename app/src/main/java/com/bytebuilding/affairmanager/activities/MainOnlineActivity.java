b08c5e4929b495da15aad9df81eb2466b3c7966f
aLT22 <altsworks@gmail.com>
2016-12-04 19:30:53 +0300

    offline client branch commit
    end to work with affair animation (when affair goes from one tab to another)
    add normal fab colors

--- app/src/main/java/com/bytebuilding/affairmanager/activities/MainOnlineActivity.java
+++ app/src/main/java/com/bytebuilding/affairmanager/activities/MainOnlineActivity.java
@@ -32,8 +32,6 @@ public class MainOnlineActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_online);
         ButterKnife.bind(this);
-        fabAddAffair.setColorFilter(getResources().getColor(R.color.color_white));
-        fabAddGroup.setColorFilter(getResources().getColor(R.color.color_white));
     }
 
     @OnClick(R.id.fab_add_affair)
