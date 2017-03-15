ee4847169a7ad0f5529d2ce6e2b2fc21aa35481a
atlas <altsworks@gmail.com>
2017-03-15 15:58:35 +0300

    Add splash screen into application

---
+++ app/src/main/java/com/bytebuilding/affairmanager/activities/SplashScreen.java
@@ -0,0 +1,15 @@
+package com.bytebuilding.affairmanager.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bytebuilding.affairmanager.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
--- app/src/main/java/com/bytebuilding/affairmanager/model/User.java
+++ app/src/main/java/com/bytebuilding/affairmanager/model/User.java
@@ -4,6 +4,7 @@ package com.bytebuilding.affairmanager.model;
  * Created by atlas on 15.03.17.
  */
 
+// TODO: 15.03.17 add more info in that model-class
 public class User {
     private String login;
     private String password;
