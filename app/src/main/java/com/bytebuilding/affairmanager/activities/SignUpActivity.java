4c6340edbc69fa0b8ee1c03df280216c8f33fb04
atlas <altsworks@gmail.com>
2017-03-24 17:33:31 +0300

    add implementation of firebase-database and user-registration

--- app/src/main/java/com/bytebuilding/affairmanager/activities/SignUpActivity.java
+++ app/src/main/java/com/bytebuilding/affairmanager/activities/SignUpActivity.java
@@ -1,6 +1,7 @@
 package com.bytebuilding.affairmanager.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -14,13 +15,19 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.bytebuilding.affairmanager.R;
+import com.bytebuilding.affairmanager.model.realm.User;
+import com.bytebuilding.affairmanager.utils.CryptoUtils;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
+import io.realm.Realm;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -34,6 +41,18 @@ public class SignUpActivity extends AppCompatActivity {
 
     private Unbinder unbinder;
 
+    private String login = "login";
+    private String password = "password";
+    private String job = "job";
+
+    private SharedPreferences preferences;
+
+    private Realm realm;
+
+    private DatabaseReference rootReference = FirebaseDatabase.getInstance()
+            .getReferenceFromUrl("https://affair-manager-8a6f9.firebaseio.com/");
+    private DatabaseReference userReference = rootReference.child("users");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,7 +62,73 @@ public class SignUpActivity extends AppCompatActivity {
 
         btnUserRegistrate.setEnabled(false);
 
+        preferences = getSharedPreferences("AffairManagerPreferences", MODE_PRIVATE);
+
+        realm = Realm.getDefaultInstance();
+
         setEditTexts();
+    }
+
+    private void setEditTexts() {
+        etUserJob = tilUserJob.getEditText();
+
+        tilUserJob.setHint(getResources().getString(R.string.signup_hint_job));
+
+        tilUserLogin.setError(getResources().getString(R.string.dialog_error_edit_text));
+        tilUserLogin.setHint(getResources().getString(R.string.signup_hint_login));
+        etUserLogin.setHint(getResources().getString(R.string.signup_hint_login));
+
+        tilUserPassword.setError(getResources().getString(R.string.dialog_error_edit_text));
+        tilUserPassword.setHint(getResources().getString(R.string.signup_hint_password));
+    }
+
+    private void saveUserToFirebase(User user) {
+        userReference.child(String.valueOf(user.getUserId())).setValue(user);
+    }
+
+    @OnClick(R.id.btn_user_accept)
+    public void onRegistrateClick() {
+        login = CryptoUtils.encrypt(CryptoUtils.KEY, CryptoUtils.VECTOR, etUserLogin.getText()
+                    .toString());
+        password = CryptoUtils.encrypt(CryptoUtils.KEY, CryptoUtils.VECTOR, etUserPassword.getText()
+                    .toString());
+        if (etUserJob.getText().length() == 0) {
+            job = "";
+        } else {
+            job = CryptoUtils.encrypt(CryptoUtils.KEY, CryptoUtils.VECTOR, etUserJob.getText()
+                    .toString());
+        }
+
+        preferences.edit().putString("login", login).apply();
+        preferences.edit().putString("password", password).apply();
+
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                Number num = realm.where(User.class).max("userId");
+                long nextID;
+                if(num == null) {
+                    nextID = 0;
+                } else {
+                    nextID = num.intValue() + 1;
+                }
+                User user = realm.createObject(User.class, nextID);
+                user.setUserLogin(login);
+                user.setUserPassword(password);
+                user.setUserOrganization(job);
+            }
+        });
+
+        saveUserToFirebase(realm.where(User.class).findAll().last());
+
+        Intent intent = new Intent(this, MainOnlineActivity.class);
+        startActivity(intent);
+        this.finish();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
 
         etUserLogin.addTextChangedListener(new TextWatcher() {
             @Override
@@ -102,26 +187,6 @@ public class SignUpActivity extends AppCompatActivity {
             public void afterTextChanged(Editable s) {
             }
         });
-    }
-
-    private void setEditTexts() {
-        etUserJob = tilUserJob.getEditText();
-
-        tilUserJob.setHint(getResources().getString(R.string.signup_hint_job));
-
-        tilUserLogin.setError(getResources().getString(R.string.dialog_error_edit_text));
-        tilUserLogin.setHint(getResources().getString(R.string.signup_hint_login));
-        etUserLogin.setHint(getResources().getString(R.string.signup_hint_login));
-
-        tilUserPassword.setError(getResources().getString(R.string.dialog_error_edit_text));
-        tilUserPassword.setHint(getResources().getString(R.string.signup_hint_password));
-    }
-
-    @OnClick(R.id.btn_user_accept)
-    public void onRegistrateClick() {
-        Intent intent = new Intent(this, MainOnlineActivity.class);
-        startActivity(intent);
-        this.finish();
     }
 
     @Override
